93955575ac98c9889e99f1e326d3b810652bda95
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-06 16:16:22 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsLibrary
+{
+    public class Employee
+    {
+        //variables
+        static int empcount;
+        int _empid,_deptno;
+        string _ename;
+        //int _deptno;
+
+        static Employee()
+        {
+            //static constructor doesn't have access specifier
+            //it is called BEFORE the first object gets created.
+            //its only called ONCE in the lifetime of the class
+            //its used to initialize variables in the class
+            //connection initializations are done in the static constructor.
+            Console.WriteLine("Static constructor called");
+            empcount = 0;
+            Console.WriteLine("currently no employees... Empcount = " + empcount );
+        }
+
+            //ctor + tab twice
+        public Employee()
+        {
+            Console.WriteLine("Default constructor...");
+            empcount += 1;
+        }
+
+        //constructors with parameters are called parameterised constructors
+        public Employee(int empid,string ename,int deptno)
+        {
+            empcount += 1;
+            _empid = empid;
+            _ename = ename;
+            _deptno = deptno;
+            //this._empid = empid;
+            //this._ename = ename;
+            //this._deptno = deptno;
+        }
+
+        public void AcceptData()
+        {
+            //Console.WriteLine("Enter employee id");
+            //_empid = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee name");
+            _ename = Console.ReadLine();
+
+            Console.WriteLine("Enter Deptno");
+            _deptno = Convert.ToInt32(Console.ReadLine());
+
+
+            //empcount += 1;
+            
+
+        }
+
+        public void DisplayData()
+        {
+            //_empid = empcount;
+            Console.WriteLine("Your employee id= " + _empid);
+            Console.WriteLine(_empid);
+            Console.WriteLine(_ename);
+            Console.WriteLine(_deptno);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPsLibrary;
+namespace OOPsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee c = new Employee();//Default constructor of teh class             instance name is c & class name is Class1
+
+            c.AcceptData();
+
+            c.DisplayData();
+
+            Console.WriteLine("-----------------------------");
+            Employee p = new Employee();
+            p.AcceptData();
+            p.DisplayData();
+
+            Console.WriteLine("------------------------------");
+
+            Employee obj = new Employee(45, "Paresh", 10);
+            obj.DisplayData();
+            Console.Read();
+        }
+    }
+}
