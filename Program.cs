0c67ea4608a7b0eb8e3c4f779fa02da195a47194
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-07 10:48:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPsLibrary;
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Products p = new Products();
+            Console.WriteLine("Enter Product ID u want to buy");
+            p.ProductID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter Name");
+            p.ProductName = Console.ReadLine();
+
+            Console.WriteLine("Enter Price");
+            p.UnitPrice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Supplier ID");
+            p.SupplierID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("U entered the following");
+
+            Console.WriteLine("Searching for your product id " + p.ProductID);//get
+            Console.WriteLine("Prodname = " + p.ProductName);
+            Console.WriteLine("Price = " + p.UnitPrice);
+            Console.WriteLine("Supplier ID=" + p.SupplierID);
+
+
+            //Console.WriteLine("Enter the way u want to give the details.. 1. default 2.parameterised");
+            //int ch = Convert.ToInt32(Console.ReadLine());
+            //switch (ch)
+            //{
+            //    case 1:
+            //        Student s = new Student();
+            //        ShowAll(s);
+
+            //        CalculatePercentage(s);
+
+
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Enter name ");
+            //        string name = Console.ReadLine();
+            //        Console.WriteLine("Enter grade");
+            //        string g = Console.ReadLine();
+            //        Student s1 = new Student(name, g);
+            //        ShowAll(s1);
+            //        //s1.AcceptMarks();
+            //        //s1.CalculateMarks();
+            //        //s1.DisplayMarks();
+
+            //        CalculatePercentage(s1);
+            //        break;
+
+                
+            //}
+            
+
+
+
+            Console.Read();
+        }
+
+        private static void ShowAll(Student s)
+        {
+            s.AcceptMarks();
+            s.CalculateMarks();
+            s.DisplayMarks();
+        }
+
+        private static void CalculatePercentage(Student s)
+        {
+            int studentMarks = s.TotalMarks;
+            int perc = studentMarks / 3;
+            Console.WriteLine(perc);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsLibrary
+{
+    
+    public class Student
+    {
+        static int rollno = 0;
+        int _rollno;
+        string _sname;
+        string _std;
+        int s1, s2, s3,tmarks;
+
+        public Student()
+        {
+            rollno += 1;
+            Console.WriteLine("enter name");
+            _sname = Console.ReadLine();
+
+        }
+
+        public Student(string sname,string grade)
+        {
+            rollno += 1;
+            this._sname = sname;
+            this._std = grade;
+        }
+
+        public void DisplayMarks()
+        {
+            Console.WriteLine("Rollno " + rollno);
+            Console.WriteLine("Name of Student= " + _sname);
+            Console.WriteLine("S1= " + s1);
+            Console.WriteLine("S2= " + s2);
+            Console.WriteLine("S3= " + s3);
+
+        }
+
+
+        public void AcceptMarks()
+        {
+            
+
+            Console.WriteLine("Enter sub1 marks");
+            s1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter sub2 marks");
+            s2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter sub3 marks");
+            s3 = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+
+        public int TotalMarks { get { return tmarks; } }
+
+        public void CalculateMarks()
+        {
+            //3 subjects---3 variables
+            //3 times -- accept marks for 3 diff sub--read/write--properties
+            //3 times----display for 3 different sub--read/write--properties
+            tmarks=s1 + s2 + s3;
+            Console.WriteLine(tmarks);
+        }
+
+
+    }
+}
