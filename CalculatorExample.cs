3d44ff1071baf14c3da08bb8fe763ae2754abbde
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-08 10:50:20 +0530

    Add files via upload

---
+++ CalculatorExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerDemoEvents
+{
+    
+    class Class1 : EventArgs
+    {
+        public static event Calculation _DoAddition;
+        public static event Calculation _DoSubtraction;
+        public static event showSquareRoot _showRoot;
+        static void Main(string[] args)
+        {
+            
+        MyClass c = new MyClass();
+        _DoAddition=new Calculation(c.Add);
+         _DoAddition.Invoke(10, 30);
+
+            _DoSubtraction = new Calculation(c.Subtraction);
+            _DoSubtraction(20, 5);
+
+            int no1 = 100;
+            _showRoot = new showSquareRoot(c.CalSquareRoot);
+            int a=_showRoot(no1);
+            Console.WriteLine(a);
+
+
+            Console.Read();
+        
+      }
+
+
+
+   
+
+    }
+}
+
