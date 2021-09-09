74df971bce624293f17009cb8e1013a501d3f216
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-09 15:14:49 +0530

    Add files via upload

---
+++ JsonDemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+namespace SerializationDemo
+{
+    class JsonDemo
+    {
+        static void Main(string[] args)
+        {
+            //JsonSerialize();
+            FileStream fs = new FileStream("customerdata.json", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string c=sr.ReadToEnd();
+            sr.Close();
+            fs.Close();
+            Customer s1=JsonConvert.DeserializeObject<Customer>(c);
+            Console.WriteLine(s1.Custid);
+            Console.WriteLine(s1.CustName);
+            Console.WriteLine(s1.CustCity);
+            Console.Read();
+
+
+        }
+
+        private static void JsonSerialize()
+        {
+            FileStream fs = new FileStream("customerdata.json", FileMode.Create, FileAccess.Write);
+
+
+            Customer c = new Customer();
+            c.Custid = 1;
+            c.CustName = "Aryan";
+            c.CustCity = "Pune";
+
+            string s1 = JsonConvert.SerializeObject(c);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(s1);
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
