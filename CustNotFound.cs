5434d0284efbbf2c1eb296ea879cc0d07e78f4d8
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-09 17:04:34 +0530

    Add files via upload

---
+++ CustNotFound.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Customer
+    {
+        int _custid;
+        public int Custid { get { return _custid; } set {
+                if (value<=0)
+                {
+                    throw new CustNotFoundException("Not found");
+                }
+                else
+                {
+                    _custid = value;
+                }
+
+            }
+
+        }
+    }
+
+
+    [Serializable]
+    public class CustNotFoundException : Exception
+    {
+        public CustNotFoundException() { }
+        public CustNotFoundException(string message) : base(message) { }
+        public CustNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected CustNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i=0, j=0, ans=0;
+            Customer c = new Customer();
+
+            try
+            {
+                Console.WriteLine("entr custid");
+                c.Custid = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(c.Custid);
+                //Console.WriteLine("entr j");
+                //j = Convert.ToInt32(Console.ReadLine());
+
+                //ans = i / j;
+                //Console.WriteLine(ans);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.Data);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine(ex.StackTrace);
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.Data);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (CustNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.Data);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.Read();
+        }
+    }
+}
